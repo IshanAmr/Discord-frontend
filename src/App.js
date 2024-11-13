8b27f7f70e5a8127fa48df263270f9500fed07c2
IshanAmr22 <ishanamrit2016@gmail.com>
2024-11-13 12:28:08 +0530

    Corrections done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<LoginPage />} />
+        {/* Example of a protected route, if needed later */}
+        {/* <Route path='/dashboard' element={<Dashboard />} /> */}
+        
+        {/* Redirect to login page when accessing root path */}
+        <Route path='/' element={<Navigate to='/login' />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/RegisterPage/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RegisterPage = async () => {
+    return (
+        <div>
+           Register Page
+        </div>
+    );
+}
+
+export default RegisterPage;
